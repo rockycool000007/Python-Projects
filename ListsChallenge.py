229afbc40a75137f750d9e6c79de995e67f29396
Brittany Gates <bcgates82@gmail.com>
2018-01-16 22:11:23 -0500

    Complete Python Masterclass - Lists Challenge
    My source code for the Lists Challenge

---
+++ ListsChallenge.py
@@ -0,0 +1,16 @@
+menu = []
+menu.append(["egg", "spam", "bacon"])
+menu.append(["egg", "sausage", "bacon"])
+menu.append(["egg", "spam"])
+menu.append(["egg", "bacon", "spam"])
+menu.append(["egg", "bacon", "sausage", "spam"])
+menu.append(["spam", "bacon", "sausage", "spam"])
+menu.append(["spam", "egg", "spam", "spam", "bacon", "spam"])
+menu.append(["spam", "egg", "sausage", "spam"])
+
+for meal in menu:
+    if not "spam" in meal:
+        your_meal = meal
+        for dish in your_meal:
+            print("Your meal consists of {0}".format(dish))
+
